4ca2eef50df7bdac665658184002090e5d154f49
yaegaki <yaegaki.fino@gmail.com>
2018-12-23 14:32:38 +0900

    整理

--- main.py
+++ main.py
@@ -13,14 +13,6 @@ import vocabulary
 
 font = sys.argv[1]
 
-# ストップワード
-# このURLのものを使用: https://qiita.com/kenmatsu4/items/9b6ac74f831443d29074
-stop_words = [ u'てる', u'いる', u'なる', u'れる', u'する', u'ある', u'こと', u'これ', u'さん', u'して',
-            u'くれる', u'やる', u'くださる', u'そう', u'せる', u'した',  u'思う',
-            u'それ', u'ここ', u'ちゃん', u'くん', u'', u'て',u'に',u'を',u'は',u'の', u'が', u'と', u'た', u'し', u'で',
-            u'ない', u'も', u'な', u'い', u'か', u'ので', u'よう', u'']
-
-
 # 使用されている用語を探し出す
 print('get vocabulary from files.')
 vocabulary_dic = vocabulary.get_vocabulary_dic()
@@ -29,11 +21,9 @@ print('calc vocabulary use rate.')
 vocabulary_use_rate = vocabulary.calc_total_use_rate(vocabulary_dic)
 
 # 全体の6割以上で使用されているものはストップワードとする
-additional_stop_words = [keyword for keyword, rate in vocabulary_use_rate.items() if rate > 0.6]
+stop_words = set([keyword for keyword, rate in vocabulary_use_rate.items() if rate > 0.6])
 # デバッグ用
-# print(additional_stop_words)
-
-stop_words2 = stop_words + additional_stop_words
+# print(stop_words)
 
 
 mask_image_pathes = [
@@ -51,8 +41,8 @@ def get_keywords_text(keywords):
     return ' '.join(keywords)
 
 settings = [
-    [stop_words, masks[0], 'all'],
-    [stop_words2, masks[1], 'part'],
+    [set(), masks[0], 'all'],
+    [stop_words, masks[1], 'part'],
 ]
 
 
@@ -79,7 +69,7 @@ def make_wordcloud(path, vocabulary, font, background_color, contour_color):
             contour_width=3,
             contour_color=contour_color,
             font_path=font,
-            stopwords=set(sw),
+            stopwords=sw,
             mask=mask,
         ).generate(keywords_text)
 
