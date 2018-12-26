7e499d85a8f145899f89f9ef87b9dace49a524c4
yaegaki <yaegaki.fino@gmail.com>
2018-12-27 02:34:15 +0900

    jsonベースに変更

--- get_keywords.py
+++ get_keywords.py
@@ -1,4 +1,5 @@
 import codecs
+import json
 from normalize_neologd import normalize_neologd
 
 def check_accept(word, cols, accept_part):
@@ -14,11 +15,15 @@ def check_accept(word, cols, accept_part):
 
 def get_keywords(mecab, path, accept_part=['名詞', '形容詞']):
     with codecs.open(path, encoding='utf-8') as f:
+        messages = json.loads(f.read())
         keywords = []
-        # 1行ずつパースしていく
-        for line in f.readlines():
+        for message_dic in messages:
+            if 'message' not in message_dic:
+                print('{}:{}'.format(path,message_dic))
+                continue
+            message = message_dic['message']
             # 正規化してからMeCabに渡す
-            rows = mecab.parse(normalize_neologd(line))
+            rows = mecab.parse(normalize_neologd(message))
             # 形態素が行ごとにあるのでそれで分割していく
             for row in rows.split('\n'):
                 cols = row.split('\t')
